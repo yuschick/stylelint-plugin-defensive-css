f12beb9f75c145dd6c00c56fcdd54a286b9d68dc
Daniel Yuschick <daniel.yuschick@gmail.com>
2023-03-24 10:24:33 +0200

    fix: prefix selector

--- src/utils/findVendorPrefixes.js
+++ src/utils/findVendorPrefixes.js
@@ -1,6 +1,8 @@
 const expression = /-[-moz-|-ms-|-o-|-webkit-|]+-/g;
 
 function findVendorPrefixes(selector) {
+  if (!selector) return false;
+
   let prefixesFound = [...selector.matchAll(expression)];
   return prefixesFound.length > 1;
 }
