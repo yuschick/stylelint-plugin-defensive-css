ddfb12c441dcd64cd114a3d6b7eee21903338cea
Daniel Yuschick <daniel.yuschick@gmail.com>
2023-12-28 19:18:55 +0200

    fix: update vendor prefix regex

--- src/utils/findVendorPrefixes.js
+++ src/utils/findVendorPrefixes.js
@@ -1,9 +1,11 @@
-const expression = /-[moz|ms|o|webkit|]+-/g;
+const expression = /-\b(moz|ms|o|webkit)\b-/g;
 
 function findVendorPrefixes(selector) {
+  console.log(selector);
   if (!selector) return false;
 
   let prefixesFound = [...selector.trim().matchAll(expression)];
+  console.log(prefixesFound);
   return prefixesFound.length > 1;
 }
 
