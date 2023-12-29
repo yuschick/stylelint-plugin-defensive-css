c492036e6e577523fd80f463e35da3bad302000b
Daniel Yuschick <yuschick@users.noreply.github.com>
2023-12-29 08:17:31 +0200

    Fix Logging Selectors Error (#29)

--- src/utils/findVendorPrefixes.js
+++ src/utils/findVendorPrefixes.js
@@ -1,11 +1,9 @@
 const expression = /-\b(moz|ms|o|webkit)\b-/g;
 
 function findVendorPrefixes(selector) {
-  console.log(selector);
   if (!selector) return false;
 
   let prefixesFound = [...selector.trim().matchAll(expression)];
-  console.log(prefixesFound);
   return prefixesFound.length > 1;
 }
 
