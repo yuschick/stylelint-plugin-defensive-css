245ecf55e3048a34226f26b7446f7926bbcb90ff
Daniel Yuschick <daniel.yuschick@gmail.com>
2024-01-22 15:09:51 +0200

    fix: duplicate vendor prefix false positive

--- src/utils/findVendorPrefixes.js
+++ src/utils/findVendorPrefixes.js
@@ -3,6 +3,8 @@ const expression = /-\b(moz|ms|o|webkit)\b-/g;
 export function findVendorPrefixes(selector) {
   if (!selector) return false;
 
-  let prefixesFound = [...selector.trim().matchAll(expression)];
-  return prefixesFound.length > 1;
+  const prefixesFound = [...selector.trim().matchAll(expression)];
+  const prefixes = new Set(prefixesFound.map((prefix) => prefix[1]));
+
+  return prefixes.size > 1;
 }
