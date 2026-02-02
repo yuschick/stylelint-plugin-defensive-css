488b8d0bab51842dfa10ebd1c3a0339e13d0aec8
Daniel Yuschick <daniel.yuschick@gmail.com>
2026-02-02 09:45:08 +0200

    chore: update rejection message for focus-visible

--- src/rules/require-focus-visible/index.ts
+++ src/rules/require-focus-visible/index.ts
@@ -7,7 +7,7 @@ export const name = 'defensive-css/require-focus-visible';
 
 export const messages = ruleMessages(name, {
   rejected: (selector: string) =>
-    `Use ":focus-visible" instead of ":focus" in selector "${selector}" for better keyboard navigation UX`,
+    `Expected ":focus-visible" instead of ":focus" in selector "${selector}" for better keyboard navigation UX`,
 });
 
 export const meta: RuleMeta = {
